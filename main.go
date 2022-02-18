bf4fa91970fa63239ec63bf6fde2bd41ed118cf6
Mihail Marian <m.marian@elsevier.com>
2022-02-18 16:25:05 +0000

    added colly

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Starting")
+
+	// Instantiate default collector
+	c := colly.NewCollector()
+	c.Visit("https://abs")
 }
