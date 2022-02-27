d42121c95e0490b4891900d8d602802ca1443342
Mihail Marian <m.marian@elsevier.com>
2022-02-27 22:32:05 +0000

    time parsing passing

--- scraper.go
+++ scraper.go
@@ -82,7 +82,7 @@ func parseCollyHtml(e ICollyHtmlElem) (FightRecord, error) {
 func parseDateTime(s string) (time.Time, error) {
 	var errOut error = nil
 
-	result, err := time.Parse("2006-01-02T00:00:00-08:00", s)
+	result, err := time.Parse("2006-01-02T15:04:05-07:00", s)
 
 	if err != nil {
 		errOut = fmt.Errorf("can't parse date from html - %v. ", err)
