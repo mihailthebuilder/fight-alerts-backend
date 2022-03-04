581b7f5477f8c505966abfb4dd4c9d72acbcb88c
Mihail Marian <m.marian@elsevier.com>
2022-03-04 18:44:42 +0000

    terraform working

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "fight_alerts_scraper_lambda_bucket" {
+resource "aws_s3_bucket" "fight_alerts_scraper_lambda" {
   bucket = "fight-alerts-scraper-lambda-bucket-prod"
 
   tags = {
@@ -26,5 +26,20 @@ resource "aws_s3_bucket" "fight_alerts_scraper_lambda_bucket" {
 
 output "fight_alerts_scraper_lambda_function" {
   description = "Name of the S3 bucket used to store function code."
-  value       = aws_s3_bucket.fight_alerts_scraper_lambda_bucket.id
+  value       = aws_s3_bucket.fight_alerts_scraper_lambda.id
+}
+
+data "archive_file" "fight_alerts_scraper_lambda" {
+  type        = "zip"
+  source_file = "../functions/bin/scraper"
+  output_path = "scraper.zip"
+}
+
+resource "aws_s3_bucket_object" "fight_alerts_scraper_lambda" {
+  bucket = aws_s3_bucket.fight_alerts_scraper_lambda.id
+
+  key    = "scraper.zip"
+  source = data.archive_file.fight_alerts_scraper_lambda.output_path
+
+  etag = filemd5(data.archive_file.fight_alerts_scraper_lambda.output_path)
 }
