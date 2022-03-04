8fcea6ca5a882531f3ada407f5753ef32255a160
Mihail Marian <m.marian@elsevier.com>
2022-03-04 21:10:32 +0000

    terraform lambda setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,3 +43,46 @@ resource "aws_s3_bucket_object" "fight_alerts_scraper_lambda" {
 
   etag = filemd5(data.archive_file.fight_alerts_scraper_lambda.output_path)
 }
+
+
+resource "aws_lambda_function" "fight_alerts_scraper_lambda" {
+  function_name = "fight-alerts-scraper-lambda-prod"
+
+  s3_bucket = aws_s3_bucket.fight_alerts_scraper_lambda.id
+  s3_key    = aws_s3_bucket_object.fight_alerts_scraper_lambda.key
+
+  runtime = "go1.x"
+  handler = "scraper"
+
+  source_code_hash = data.archive_file.fight_alerts_scraper_lambda.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "fight_alerts_scraper_lambda" {
+  name = "/aws/lambda/${aws_lambda_function.fight_alerts_scraper_lambda.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
