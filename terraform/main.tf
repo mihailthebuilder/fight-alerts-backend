8f9a6074bd1750925b3dd4ba1666cfea2268f107
Mihail Marian <m.marian@elsevier.com>
2022-03-15 19:45:55 +0000

    another creds attempt

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ locals {
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = ["/root/.aws/credentials"]
+  shared_credentials_file = "/root/.aws/credentials"
 }
 
 resource "aws_s3_bucket" "fight_alerts_scraper_lambda" {
